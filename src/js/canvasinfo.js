05b6696138262e5891738cb58f7d47ae61bf8c5d
Somka000 <51973243+Somka000@users.noreply.github.com>
2023-01-16 10:09:56 +0100

    lol

--- src/js/canvasinfo.js
+++ src/js/canvasinfo.js
@@ -2,21 +2,19 @@ function rozmiar(){
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
     }
-    const ctx = canvas.getContext("2d");
+    const ctx = document.getElementById("canvas").getContext("2d");
     var image = new Image();
     image.src = "src/images/mapa.png";
-    image.onload = function(){
+    var szerokośćokna = window.screen.width;
+    var wysokośćokna = window.screen.height;
+    var różnicarozdzielczości = szerokośćokna / image.width;
+    function dostosowanie(){
+        if (szerokośćokna > image.width) {
+            canvas.width = szerokośćokna * różnicarozdzielczości;
+            canvas.height = wysokośćokna * różnicarozdzielczości;
+        }
+image.onload = function(){
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-    }
-    rozmiar();
-    window.addEventListener("resize", function(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    const imageRatio = image.width / image.height;
-    const canvasRatio = canvas.width / canvas.height;
-    if (canvasRatio > imageRatio) {
-        ctx.drawImage(image, 0, 0, canvas.width, canvas.width / imageRatio);
-    } else {
-        ctx.drawImage(image, 0, 0, canvas.height * imageRatio, canvas.height);
-    }
-});
+}};
+dostosowanie();
+window.addEventListener("resize", dostosowanie);
